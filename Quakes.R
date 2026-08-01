367caf893bff720e2a65e48445b1d46d84f3573e
pp46902022-tech <pp46902022@gmail.com>
2026-08-01 21:40:07 +0530

    Add files via upload

---
+++ Quakes.R
@@ -0,0 +1,18 @@
+e_quakes<- datasets::quakes
+head(quakes,10)
+tail(quakes,10)
+quakes[,c(1,2)]
+quakes$mag
+df<-quakes[,-5]
+summary(quakes[,c(1,2,3)])
+quakes$stations
+plot(quakes$depth)
+plot(quakes$depth,quakes$mag,type="b")
+plot(quakes$mag,main='magnitude of earthquake',xlab='magnitude',ylab='instances',col='red')
+barplot(quakes$mag,main='magnitude of earthquake',xlab='magnitude',ylab='instances',col='blue')
+hist(quakes$depth)
+hist(quakes$mag,main='magnitude',xlab='magnitude',col='red')
+boxplot(quakes$mag)
+boxplot.stats(quakes$mag)$out
+plot(quakes$depth,quakes$mag)
+plot(quakes)
